81e3824733e2c521bc910802d63e0c636c61d53a
АлшериевС <@610-10>
2022-07-14 21:11:22 +0600

    Add project files.

---
+++ OnlineStoreAPI/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OmlineStore.Model;
+using Services.InterFaces;
+
+namespace OnlineStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductInterFase _productService;
+
+        public ProductController(ProductInterFase productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("products")]
+        public async Task<IActionResult> GetProducts()
+        {
+            var result = await _productService.GetProducts();
+            return Ok(result);
+        }
+
+        [HttpPut("createProducts")]
+        public async Task<IActionResult> CreateProduct([FromBody] Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        [HttpPost("updateProducts")]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services.InterFaces/ProductInterFase.cs
@@ -0,0 +1,15 @@
+﻿using OmlineStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.InterFaces
+{
+    public interface ProductInterFase
+    {
+        Task<Product> GetProduct();
+        Task<List<Product>> GetProducts();
+    }
+}
