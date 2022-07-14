bd4881c06f047b539bf91db71406227739e8b9e3
Сабит Курманалиев <sabit_fox@mail.ru>
2022-07-14 21:42:05 +0600

    Add base Product Repository

---
+++ OnlineStore.Repo.Impl/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using OmlineStore.Model;
+using OnlineStore.Repo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Repo.Impl
+{
+    public class ProductRepository : IProductRepository
+    {
+        public async Task DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Product>> GetProduct(int? top = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> InsertProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OnlineStore.Repo.Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using OmlineStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Repo.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductById(int id);
+        Task<List<Product>> GetProduct(int? top = null);
+        Task<int> InsertProduct(Product product);
+        Task UpdateProduct(Product product);    
+        Task DeleteProduct(int id);
+
+    }
+}
